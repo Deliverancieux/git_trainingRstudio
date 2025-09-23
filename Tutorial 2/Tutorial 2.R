74e21eed45b806f048346d4e82533a8d5a11860f
Deliverancieux <genius242424@gmail.com>
2025-09-23 13:40:42 +0000

    Tutorial 2 WIP

---
+++ Tutorial 2/Tutorial 2.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+
+data <- read.csv("Tutorial 2/Cars_data.csv")
+
+data$Make <- as.factor(data$Make)
+data$Year <- as.factor(data$Year) #Year as Factor, as scale is problematic. Maybe 'Age' would be a better integer variable?
+data$Engine.Fuel.Type <- as.factor(data$Engine.Fuel.Type)
+data$Engine.Cylinders <- as.factor(data$Transmission.Type)
+data$Driven_Wheels <- as.factor(data$Driven_Wheels)
+data$Market.Category <- as.factor(data$Market.Category)
+data$Vehicle.Size <- as.factor(data$Vehicle.Size)
+data$Vehicle.Style <- as.factor(data$Vehicle.Style)
+
+data$Model <- NULL #IDK this seems stupid to do IMO, I see no real reason to delete. Maybe it has too much correlation with Make, or something.
+
+set.seed(42)
+n <- dim(data)[1]
+train_ind <- sample(1:n, n*0.7)
+
+train_data <- data[train_ind,]
+test_data <- data[-train_ind,]
